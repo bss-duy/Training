53be96fbd077abd9dfc5c3b6a025614fae01d466
Duy Nguyen <duy.nguyen@beesightsoft.com>
2017-02-06 17:39:04 +0700

    Third commit feb 6

--- set_build_version.sh
+++ set_build_version.sh
@@ -1,2 +1,6 @@
-Nguyen bao duy edited
-Nguyen bao duy edited
+This is the line that another person edited`
+This is the line that i have edited
+
+1/ Resolve conflict using Mine
+2/ Resolve conflict using Thiers
+3/ Resolve manually
